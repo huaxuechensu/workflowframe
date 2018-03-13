ce6bbd1155aeed7dfbb0f192ba8746d1caf58196
chendoudou <hua-xue21@163.com>
2018-03-13 08:46:03 +0800

    project init. Springboot and gradle. clean project .

---
+++ workflow-web/src/main/java/com/henan/h3c/solution/workflowweb/WorkflowWebApplication.java
@@ -0,0 +1,12 @@
+package com.henan.h3c.solution.workflowweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowWebApplication.class, args);
+	}
+}
---
+++ workflow-web/src/test/java/com/henan/h3c/solution/workflowweb/WorkflowWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.henan.h3c.solution.workflowweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkflowWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
